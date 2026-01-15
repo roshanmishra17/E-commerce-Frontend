44917ba0815c6caf4576cf5b12cb5f73a1582767
akashway <akashmishraway@gmail.com>
2026-01-15 16:29:25 +0530

    Create Routes for Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/login";
 import SignUp from "./Pages/SignUp";
+import Products from "./Pages/Products"
 
 
 
@@ -10,8 +11,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Products/>}/>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/products" element={<Products/>}/>
         </Routes>
       </BrowserRouter>
     </>
