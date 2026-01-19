a3fd34b3f310387a4041fb86e307c508fdfbac13
akashway <akashmishraway@gmail.com>
2026-01-19 22:04:57 +0530

    Added CSS file for products

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,17 +2,12 @@ import NavBar from "./NavBar";
 import API from "../api/axios";
 import { useState,useEffect } from "react";
 import { Link} from "react-router-dom";
-
+import '../CSS/Products.css'
 
 export default function Products(){
     const getProductsApi = async (params = {}) => {
         const res = await API.get("/products", { params });
         return res.data; 
-    };
-
-    const getProductBySlugApi = async (slug) => {
-        const res = await api.get(`/products/slug/${slug}`);
-        return res.data;
     };
 
     const getCategoriesApi = async () => {
@@ -62,95 +57,66 @@ export default function Products(){
         loadCategories();
     }, []);
 
-    console.log(products)
-
     if (loading) return <p>Loading products...</p>;
     if (error) return <p style={{ color: "red" }}>{error}</p>;
 
     return (
-        <div style={{ padding: "20px" }}>
-            <div style={categoryBarStyle}>
-                <button
-                    onClick={() => loadProducts(1, null)}
-                    style={!selectedCategory ? activeCategoryStyle : {}}
-                >
-                    All
-                </button>
-
-                {categories.map((cat) => (
+        <>
+            <NavBar/>
+            <div className="products-page">
+                <div className="category-bar">
                     <button
-                        key={cat.id}
-                        onClick={() => loadProducts(1, cat.id)}
-                        style={selectedCategory === cat.id ? activeCategoryStyle : {}}
+                        onClick={() => loadProducts(1, null)}
+                        className={!selectedCategory ? "active" : ""}
                     >
-                        {cat.name}
-                    </button>
-                ))}
-            </div>
-
-            <h2>Products</h2>
-            <div style={gridStyle}>
-                {products.map((product) => (
-                    
-                    <div key={product.id} style={cardStyle}>
-                    <p>{product.category.name}</p>
-                        <img
-                            src={product.image_url || "https://via.placeholder.com/200"}
-                            alt={product.name}
-                            style={imgStyle}
-                        />
-
-                        <h3>{product.name}</h3>
-                        <p>₹ {product.price}</p>
-
-                        <Link to={`/products/${product.slug}`}>View</Link>
-                    </div>
-                ))}
-            </div>
-
-            {pagination && (
-                <div style={{ marginTop: "20px", display: "flex", gap: "10px" }}>
-                    <button disabled={page === 1 }onClick={() => loadProducts(page - 1)}>
-                        Prev
+                        All
                     </button>
 
-                    <span>
-                        Page {pagination.page} of {pagination.total_pages}
-                    </span>
-
-                    <button disabled={page === pagination.total_pages} onClick={() => loadProducts(page + 1)}>
-                        Next
-                    </button>
+                    {categories.map((cat) => (
+                        <button
+                            key={cat.id}
+                            onClick={() => loadProducts(1, cat.id)}
+                            className={selectedCategory === cat.id ? "active" : ""}                        >
+                            {cat.name}
+                        </button>
+                    ))}
                 </div>
-            )}
-        </div>
+
+                <h2 className="products-title">Products</h2>
+                <div className="products-grid">
+                    {products.map((product) => (
+                        
+                        <div key={product.id} className="product-card">
+                        <p className="product-category">{product.category.name}</p>
+                            <img
+                                src={product.image_url || "https://via.placeholder.com/200"}
+                                alt={product.name}
+                            />
+
+                            <h3>{product.name}</h3>
+                            <p className="price">₹ {product.price}</p>
+
+                            <Link to={`/products/${product.slug}`} className="view-btn">View</Link>
+                        </div>
+                    ))}
+                </div>
+
+                {pagination && (
+                    <div className="pagination">
+                        <button disabled={page === 1 } onClick={() => loadProducts(page - 1)}>
+                            Prev
+                        </button>
+
+                        <span>
+                            Page {pagination.page} of {pagination.total_pages}
+                        </span>
+
+                        <button disabled={page === pagination.total_pages} onClick={() => loadProducts(page + 1)}>
+                            Next
+                        </button>
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
-const gridStyle = {
-  display: "grid",
-  gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-  gap: "20px",
-};
-
-const cardStyle = {
-  border: "1px solid #ddd",
-  padding: "10px",
-  borderRadius: "5px",
-};
-
-const imgStyle = {
-  width: "100%",
-  height: "150px",
-  objectFit: "cover",
-};
-const categoryBarStyle = {
-  display: "flex",
-  gap: "10px",
-  marginBottom: "20px",
-  flexWrap: "wrap"
-};
-
-const activeCategoryStyle = {
-  fontWeight: "bold",
-  textDecoration: "underline"
-};
